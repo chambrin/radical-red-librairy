e43de1f7bc713fe03c18b3c6d886455eaac3917d
chambrin <achambrin@outlook.fr>
2024-04-13 16:02:02 +0200

    🐛 405 bug fix

--- src/provider/ApolloWrapper.jsx
+++ src/provider/ApolloWrapper.jsx
@@ -11,7 +11,7 @@ import {
 function makeClient() {
     const httpLink = new HttpLink({
         // Url graphql server
-        uri: "process.env.API_URL "|| "http://localhost:3000/api/graphql",
+        uri: process.env.API_URL || "http://localhost:3000/api/graphql",
         fetchOptions: { cache: "no-store" },
     });
 
