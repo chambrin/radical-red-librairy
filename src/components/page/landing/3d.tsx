f1922e03d78c2e4709cf7546ecc5f6125b7e1a9f
chambrin <achambrin@outlook.fr>
2024-04-16 09:56:34 +0200

    ➕Spline integration

---
+++ src/components/page/landing/3d.tsx
@@ -0,0 +1,8 @@
+'use client'
+import Spline from '@splinetool/react-spline';
+
+export default function SceneGameboy() {
+    return (
+        <Spline className="absolute" style={{ left: '-40px', zIndex: '-1' }} scene="https://prod.spline.design/i5hbMaCnk-I6ir44/scene.splinecode" />
+    );
+}
